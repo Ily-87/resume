41a46af2f205b2f0bb698f0dad4fdd3146087047
Ily-87 <39122989+Ily-87@users.noreply.github.com>
2019-01-03 12:36:05 +0100

    Add files via upload

---
+++ plan/src/views/home.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App" :datum="new Date().toString()"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Future from '@/components/Future.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
